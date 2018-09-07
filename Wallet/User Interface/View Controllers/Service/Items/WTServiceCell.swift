54d20e8d42ccb32af1e226c388c481c04f200bb3
LordArianz <morteza758@gmail.com>
2018-09-08 02:32:42 +0430

    wallet tab + service

---
+++ Wallet/User Interface/View Controllers/Service/Items/WTServiceCell.swift
@@ -0,0 +1,13 @@
+//
+//  WTServiceCell.swift
+//  Wallet
+//
+//  Created by S.Morteza Ezzabady on 9/8/18.
+//  Copyright © 2018 Ebrahim Tahernejad. All rights reserved.
+//
+
+import UIKit
+
+class WTServiceCell: UICollectionViewCell {
+    
+}
